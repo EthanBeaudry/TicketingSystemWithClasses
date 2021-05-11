1f982b0780d32b429026bef42400952998185117
Ethan <EthanBWctc@gmail.com>
2021-05-10 19:40:50 -0500

    TicketsWithClasses

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace TicketingClasses
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Welcome to the Ticketing System");
+            Console.Write("Would you like to create a ticket (Y/N)? ");
+            char choice = Console.ReadLine().ToUpper()[0];
+            string file = "Tickets.csv";
+            StreamWriter sw = new StreamWriter(file);
+            Ticket ticket = new Ticket();
+
+            do
+            {
+                Console.Write("TicketNumber: ");
+                ticket.ticketNumber = Console.ReadLine();
+
+                Console.Write("Summary: ");
+                ticket.ticketSummary = Console.ReadLine();
+
+                Console.Write("Status ");
+                ticket.ticketStatus = Console.ReadLine();
+
+                Console.Write("Priority ");
+                ticket.ticketPriority = Console.ReadLine();
+
+                Console.Write("Submitter ");
+                ticket.ticketSubmitter = Console.ReadLine();
+
+                Console.Write("Assigned: ");
+                ticket.ticketAssignedTo = Console.ReadLine();
+
+                Console.Write("add a watcher? (Y/N)? ");
+                char addWatcher = Console.ReadLine().ToUpper()[0];
+                
+                while (addWatcher != 'N')
+                {
+                    Console.Write("Enter the name of the watcher: ");
+                    string addName = Console.ReadLine();
+                    ticket.ticketWatchers.Add(addName);
+                    Console.Write("Would you like to add another watcher (Y/N)? ");
+                    addWatcher = Console.ReadLine().ToUpper()[0];
+
+                } 
+
+                sw.WriteLine(ticket.Display());
+                sw.Close();
+                Console.Write("Would you like to create another ticket (Y/N)? ");
+                choice = Console.ReadLine().ToUpper()[0];
+
+            } while (choice != 'N');
+            Console.WriteLine("Would you like to read the tickets? (Y/N)?");
+            choice = Console.ReadLine().ToUpper()[0];
+            if (choice != 'N')
+            {
+                 StreamReader sr = new StreamReader(file);
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                            
+                        }
+                        sr.Close();
+
+            }
+            else
+            {
+                Console.WriteLine("thank you for using the ticketing system");
+            }
+           
+        }
+    }
+}
---
+++ TIcket.cs
@@ -0,0 +1,40 @@
+using System; 
+using System.Collections.Generic;
+
+namespace TicketingClasses
+{
+    public class Ticket
+    {
+        public string ticketNumber {get; set;}
+        public string ticketSummary {get; set;}
+        public string ticketStatus {get; set;}
+        public string ticketPriority {get; set;}
+        public string ticketSubmitter {get; set;}
+        public string ticketAssignedTo {get; set;}
+        public List<string> ticketWatchers {get; set;}
+
+        public Ticket()
+        {
+            ticketWatchers = new List<string>();
+        }
+
+        public string displayTicketNames()
+        {
+            string names = "";
+            for (int i = 0; i < ticketWatchers.Count; i++)
+            {
+                if (i < ticketWatchers.Count - 1)
+                {
+                    names += ticketWatchers[i] + "|";
+                }
+                else names += ticketWatchers[i];
+            }
+            return names;
+        }
+
+        public string Display()
+        {
+            return $"{ticketNumber},{ticketSummary},{ticketStatus},{ticketPriority},{ticketSubmitter},{ticketAssignedTo},{displayTicketNames()}";
+        }
+    }
+}
